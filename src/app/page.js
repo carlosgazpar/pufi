f54e3b559c2b5daa0562013facc6b4b0488d7274
Juan Carlos Barrios <tornillosbabe@hotmail.com>
2023-08-02 20:32:43 -0600

    feat:Creation of main banners slider.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import CategoriesCards from "@/components/CategoriesCards";
-
+import MainSlider from "@/components/MainSlider";
 
 export default function Home() {
   return (
     <>
+      <MainSlider />
       <CategoriesCards />
     </>
-  )
+  );
 }
---
+++ src/components/MainSlider.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Button from "@/common/Button";
+import "@/styles/components/MainSlider.scss";
+import ReactElasticCarousel from "react-elastic-carousel";
+const MainSlider = () => {
+  return (
+    <div className="main-slider">
+      <ReactElasticCarousel showArrows={false} enableAutoPlay={true} autoPlaySpeed={3000}>
+        <div
+          className="main-slider-item"
+          style={{ backgroundImage: "url(/assets/img/main-slider1.jpg)" }}
+        >
+          <h1>
+            Estár cómodo,
+            <br />
+            nunca fue tan fácil
+          </h1>
+          <Button link="" text="Shop" />
+        </div>
+        <div
+          className="main-slider-item"
+          style={{ backgroundImage: "url(/assets/img/main-slider1.jpg)" }}
+        >
+          <h1>
+            Estár cómodo,
+            <br />
+            nunca fue tan fácil
+          </h1>
+          <Button link="" text="Shop" />
+        </div>
+        <div
+          className="main-slider-item"
+          style={{ backgroundImage: "url(/assets/img/main-slider1.jpg)" }}
+        >
+          <h1>
+            Estár cómodo,
+            <br />
+            nunca fue tan fácil
+          </h1>
+          <Button link="" text="Shop" />
+        </div>
+      </ReactElasticCarousel>
+    </div>
+  );
+};
+
+export default MainSlider;
